c84f4f8ef3e83368ee4f6771302cfea2323362a6
YongSangYou <dydtkd113@gmail.com>
2022-01-22 17:12:07 +0900

    초기 세팅

---
+++ src/GlobalStyle.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  a, body, div, em, h1, h2, h3, h4, h5, h6, img, label, li, span, table, tbody, td, th, thead, tr, ul {
+      margin: 0;
+      padding: 0;
+      border: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  *:before, *:after {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  body {
+    font-size: 14px;
+    line-height: 1.42857143;
+    color: #333333;
+    background-color: #f8f8fa;
+  }
+  html {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    font-family: sans-serif;
+    -webkit-text-size-adjust: 100%;
+  }
+`
+
+export default GlobalStyle;
